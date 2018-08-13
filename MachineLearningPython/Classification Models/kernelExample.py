2a8c78cad9fe37695592b13871b003510dba3929
dieguito3076 <diegovelazqu@hotmail.com>
2018-08-12 23:46:01 -0500

    actualizando

---
+++ MachineLearningPython/Classification Models/kernelExample.py
@@ -0,0 +1,6 @@
+  import numpy as np
+  from matplotlib import pyplot as plt
+
+  #inner product = dot product in numpy
+  #A similarity function which takes the similarities between two inputs and two outputs, using the inner product.
+  #Use a kernel to transform our feature space.
