24b12e1d278866efec5cdb69706efdf7b207be5e
dieguito3076 <diegovelazqu@hotmail.com>
2018-11-22 00:34:41 -0600

    modificacion al programa

--- Geneticos/MetodosLinealesOptimizacionAlgoritmosGeneticos.py
+++ Geneticos/MetodosLinealesOptimizacionAlgoritmosGeneticos.py
@@ -1,6 +1,14 @@
 import random
-from matplotlib import pyplot as plt
+import numpy as np
 
+from sympy import *
+x = Symbol('x')
+y = Symbol('y')
+
+from matplotlib import pyplot as plt
+from mpl_toolkits.mplot3d import axes3d
+
+#Metodo de optimizacion de Newton
 def funcion_fx(x):
     return x ** 4 + 6 *x **3 + 2 * x ** 2 - x
 def primera_derivada_funcion_fx(x):
@@ -23,5 +31,28 @@ def graficar(funcion, punto):
     plt.plot(punto, funcion(punto),color = "green", marker = "o")
     plt.show()
 punto_critico = Metodo_De_Newton(primera_derivada_funcion_fx, segunda_derivada_funcion_fx)
-graficar(funcion_fx, punto_critico)
-print punto_critico
+#graficar(funcion_fx, punto_critico)
+print "El punto critico es: "+str(punto_critico)
+
+ #**************************************************************************
+#fig = plt.figure()
+#ax = fig.add_subplot(111,projection = '3d')
+#z =  np.random.random((1,150))
+#x =  np.random.random((1,150))
+#y =  np.random.random((1,150))
+#ax.plot_wireframe(x,y,z)
+
+#Metodo de optimizacion de ascenso de maxima pendiente
+def funcion_en_z_y_x(x, y):
+    return -(x+2) ** 2 - (y-1)**2
+def primera_derivada_funcion_yxz(x, y):
+    return [-2*(x+2), -2*(y-1)]
+def Metodo_Optimizacion_ascensoPendiente(funcion_inicial, primera_derivada):
+    x1 =  [random.random()*10, random.random()*10]
+    evaluacion_gradiente = primera_derivada(x1[0], x1[1])
+    to = solve(diff(-(x1[0] + x * evaluacion_gradiente[0] - 5)**2 -(x1[1] + x * evaluacion_gradiente[1]-3)**2),x)
+    nuevo_punto = [x1[0] + (to[0] * evaluacion_gradiente[0]), x1[1] + (to[0] * evaluacion_gradiente[1])]
+    evaluacion_gradiente = primera_derivada(nuevo_punto[0], nuevo_punto[1])
+    return nuevo_punto
+
+print Metodo_Optimizacion_ascensoPendiente(funcion_en_z_y_x,primera_derivada_funcion_yxz)
