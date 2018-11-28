c0158ca94eb21ff9e9bd5055724d373e886b0274
dieguito3076 <diegovelazqu@hotmail.com>
2018-11-28 00:07:09 -0600

    algoritmo terminado#

--- Geneticos/combinatoria_AG.py
+++ Geneticos/combinatoria_AG.py
@@ -1,55 +1,8 @@
-import numpy as np
-esquema = "1**"
-
-def crear_poblacion(esquema):
-  count = 0
-  #Evaluacion de '*' en la cadena
-  for i in range(0,len(esquema)):
-    if(esquema[i] == "*"):
-      count = count + 1
-  #Creacion de la matriz de zeros
-  s = (2 ** count)
-  combinatoria = np.zeros(s)
-  #Declarando matriz que tendra la poblacion de cadenas binarias
-  poblacion = [esquema] * (s)
-
-  #Meter los numeros binarios a la matriz
-  for i in range(0,len(combinatoria)):
-    combinatoria[i] = bin(i)[2:]
-  len_cadenas = len(str(int(combinatoria[len(combinatoria)-1])))
-
-  #Ajustar todos los numeros binarios para que tengan el mismo numero de caracteres
-
-  for i in range(0,len(combinatoria)):
-    numero = str(int(combinatoria[i]))
-    zeros = int(len_cadenas - (len(str(int(combinatoria[i]))))) * '0'
-    nuevo_numero = zeros + numero
-    #Aqui se crea un elemento de la poblacion con su combinatoria de bits
-
-    numero_final = ""
-    posicion = 0
-    for a in range(0,len(esquema)):
-      if(poblacion[i][a] != '*'):
-        numero_final = numero_final + poblacion[i][a]
-      elif(poblacion[i][a] == '*'):
-        numero_final = numero_final + nuevo_numero[posicion]
-        posicion = posicion + 1
-    poblacion[i] = numero_final
-    nuevo_numero = ""
-  return poblacion
-
-print(crear_poblacion(esquema))
-
-
-
-
-
-#*****************************************************************************
-
+#-*- coding: utf-8 -*-
 import numpy as np
 import random
-esquema = "1**********1"
-
+from matplotlib import pyplot as plt
+esquema = "1**"
 def crear_poblacion(esquema):
   count = 0
   #Evaluacion de '*' en la cadena
@@ -86,27 +39,13 @@ def crear_poblacion(esquema):
     poblacion[i] = numero_final
     nuevo_numero = ""
   return poblacion
+#*****************************************************************************
 
-
-def retornar_indice(elemento, matriz):
-  indice = 0
-  for i in range(0,len(matriz)):
-    if(matriz[i]==elemento):
-      return indice
-    else:
-      indice = indice + 1
+esquema = "1**********1"
 
 poblacion = (crear_poblacion(esquema))
-print(poblacion)
 print("Tamano de combinatoria de esquema: "+str(len(poblacion)))
-numero_de_muestra = int(random.random() * len(poblacion) / 4)
-print("Tamano de poblacion inicial: " + str(numero_de_muestra))
-print("Una posible solucion está en el indice: "+ str(retornar_indice('110100001111', poblacion)))
-
-#Obteniendo la muestra con la que trabajara el algoritmo genetico
-poblacion_Muestra = poblacion[:numero_de_muestra]
-print(poblacion_Muestra)
-
+poblacion_muestra = poblacion[300:400]
 #Definiendo matriz de hijos
 matriz_hijos = {"A":["B","C"],"B":["A","E","D"],"C":["A","D","G"],"D":["B","C","I"],"E":["B","F"],"F":["L","K","J","I"],"G":["C","H"],"H":["G","I"],"I":["F","J","D","H"],"J":["F","K","I"],"K":["F","J","L"],"L":["F","K"]}
 
@@ -122,21 +61,25 @@ class Algoritmo_Genetico:
     self.numero_generacion = 0
     #Matriz que contendra las medidas fitness de cada elemento de la poblacion
     self.matriz_medidas_fit = np.zeros(len(self.poblacion))
+    #Población que tendrá a los hijos provisionales
+    self.poblacion_con_hijos = poblacion
     #Generaciones que quiero que haga el algoritmo
-    self.total_generations = 250
-
-  def funcion_fitness(self, elemento):
+    self.total_generations = 50
+  def funcion_fitness(self, hijo):
+    elemento = hijo
     nodo_padre = self.matriz_apoyo_letras[0] #El nodo padre inicial es A
-    for i in range(1, len(elemento)):
-      if(elemento[i] == 1):
+    medida_fit = 0
+    i = 1
+    while(i < len(elemento)):
+      if(elemento[i] ==  '1'):
         nodo_hijo = self.matriz_apoyo_letras[i]
         if(nodo_hijo in self.matriz_hijos[nodo_padre]):
           medida_fit = medida_fit + 1
           nodo_padre = self.matriz_apoyo_letras[i]
-          if(nodo_padre == self.letra_stop):
-            medida_fit = 10
+        else:
+          i = len(elemento) + 1
+      i = i + 1
     return medida_fit
-
   def retornar_indice(self,elemento, matriz):
     indice = 0
     for i in range(0,len(matriz)):
@@ -144,21 +87,79 @@ class Algoritmo_Genetico:
         return indice
       else:
         indice = indice + 1
-
+  def bernouli_selection(self):
+      hijos = []
+      i = 0
+      while(i != 2):
+          hijo = int(random.random() * len(self.poblacion))
+          random_range = random.random() * 100
+          if(random_range < self.matriz_medidas_fit[hijo]):
+              hijos.append(self.poblacion[hijo])
+              i = i+1
+      return hijos[0], hijos[1]
+  def cruzamiento_punto_de_corte(self, padre, madre):
+      punto_de_corte = int(random.random() * len(padre))
+      adn1 = str(padre[:punto_de_corte])
+      adn2 = str(madre[punto_de_corte:])
+      return str(adn1+adn2)
+  def generate_0_1(self):
+      return random.choice('01')
+  def mutacion(self, son):
+      hijo = son
+      val_en_cadena = '1'
+      for i in range(1, len(hijo)):
+          rand_num = random.random()
+          if(rand_num < self.mutation_rate):
+              nuevo_alelo = self.generate_0_1()
+              val_en_cadena = val_en_cadena + nuevo_alelo
+          else:
+              val_en_cadena = val_en_cadena + hijo[i]
+      return str(val_en_cadena)
   def evolve_main(self):
     for i in range(0,self.total_generations):
-
       #Asignando todas las medidas fitness
+      total_de_medidas_fitness = 0
       for u in range(0,len(self.poblacion)):
-        self.matriz_medidas_fit[u] = self.funcion_fitness(poblacion[u])
+        self.matriz_medidas_fit[u] = self.funcion_fitness(self.poblacion[u])
+        total_de_medidas_fitness = total_de_medidas_fitness + self.funcion_fitness(self.poblacion[u])
+      #Transformar medidas fitness a porcentajes
+      for h in range(0,len(self.matriz_medidas_fit)):
+          self.matriz_medidas_fit[h] = int(float(self.matriz_medidas_fit[h] * 100)/float(total_de_medidas_fitness))
 
       #Elitismo (En la posicion cero se guardará el mejor elemento de la generacion pasada)
       indice_elemento_max = self.retornar_indice(max(self.matriz_medidas_fit),self.matriz_medidas_fit)
       elemento_max_fitness = self.poblacion[indice_elemento_max]
-      self.poblacion[0] = elemento_max_fitness
+      self.poblacion_con_hijos[0] = elemento_max_fitness
+
       #Main del algoritmo genetico
-      for a in range(1,len(poblacion)):
+      for a in range(1,len(self.poblacion)):
         padre, madre = self.bernouli_selection()
-        hijo = self.cruzamiento(padre, madre)
+        hijo = self.cruzamiento_punto_de_corte(padre, madre)
         hijo = self.mutacion(hijo)
-        self.poblacion[i] = hijo
+        #El problema de las matrices está aqui
+        self.poblacion_con_hijos[a] = hijo
+      self.poblacion = self.poblacion_con_hijos
+
+
+
+poblacion_definida = ['110110001111','110100001111','110001001111','101100111111']
+AG = Algoritmo_Genetico(poblacion_muestra, 'L', 0.2, matriz_hijos)
+print "La poblacion inicial: " + str(AG.poblacion)
+matriz1_fit = np.zeros(len(AG.poblacion))
+matriz2_fit = np.zeros(len(AG.poblacion))
+matriz_numeracion = np.zeros(len(AG.poblacion))
+for i in range(0,len(AG.poblacion)):
+    #print AG.funcion_fitness(AG.poblacion[i])
+    matriz1_fit[i] = AG.funcion_fitness(AG.poblacion[i])
+    matriz_numeracion[i] = i+1
+plt.plot(matriz_numeracion,matriz1_fit,marker ='*', color='red')
+
+AG.evolve_main()
+
+print "La poblacion final: " + str(AG.poblacion)
+for i in range(0,len(AG.poblacion)):
+    #print AG.funcion_fitness(AG.poblacion[i])
+    matriz2_fit[i] = AG.funcion_fitness(AG.poblacion[i])
+plt.plot(matriz_numeracion,matriz2_fit,marker ='*', color='green')
+
+plt.show()
