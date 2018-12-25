e6ff3300b7e810922152078abb2ce43780e78399
dieguito3076 <diegovelazqu@hotmail.com>
2018-12-24 19:10:42 -0600

    geneticos

--- Geneticos/MetodosLinealesOptimizacionAlgoritmosGeneticos.py
+++ Geneticos/MetodosLinealesOptimizacionAlgoritmosGeneticos.py
@@ -1,5 +1,6 @@
 import random
 import numpy as np
+import math
 
 from sympy import *
 x = Symbol('x')
@@ -10,11 +11,11 @@ from mpl_toolkits.mplot3d import axes3d
 
 #Metodo de optimizacion de Newton
 def funcion_fx(x):
-    return x ** 4 + 6 *x **3 + 2 * x ** 2 - x
+    return x ** 3 * math.sin(x)+ x + 2
 def primera_derivada_funcion_fx(x):
-    return 4 * x **3 + 18 * x ** 2 + 4 * x - 1
+    return 3 * x**2 * sin(x) + x ** 3 * math.cos(x) + 1
 def segunda_derivada_funcion_fx(x):
-    return 12 * x ** 2 + 36 * x + 4
+    return -x *((x**2 - 6) * math.sin(x) - 6 * x * math.cos(x))
 def Metodo_De_Newton(deriv1,deriv2):
     x1 = random.random() * 10
     while(deriv1(x1) != 0):
@@ -44,11 +45,12 @@ print "El punto critico es: "+str(punto_critico)
 
 #Metodo de optimizacion de ascenso de maxima pendiente
 def funcion_en_z_y_x(x, y):
-    return -(x+2) ** 2 - (y-1)**2
+    return (x+4) ** 2 - (y-7)**2
 def primera_derivada_funcion_yxz(x, y):
-    return [-2*(x+2), -2*(y-1)]
+    return [2*(x+4), -2*(y-7)]
 def Metodo_Optimizacion_ascensoPendiente(funcion_inicial, primera_derivada):
-    x1 =  [random.random()*10, random.random()*10]
+    #x1 =  [random.random()*10, random.random()*10]
+    x1 = [1,1]
     evaluacion_gradiente = primera_derivada(x1[0], x1[1])
     to = solve(diff(-(x1[0] + x * evaluacion_gradiente[0] - 5)**2 -(x1[1] + x * evaluacion_gradiente[1]-3)**2),x)
     nuevo_punto = [x1[0] + (to[0] * evaluacion_gradiente[0]), x1[1] + (to[0] * evaluacion_gradiente[1])]
